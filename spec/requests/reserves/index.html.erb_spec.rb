a8eb96cced86e51ac3a0105c4e261be2439ee294
looh16 <langacustodio@gmail.com>
2022-12-11 13:54:16 +0200

    Fix errors

--- spec/requests/reserves/index.html.erb_spec.rb
+++ spec/requests/reserves/index.html.erb_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe 'Reserves', type: :request do
   let(:product) { Product.create(model: 'vespa', color: 'blue', description: 'vespa', price: 200, user_id: user.id) }
   let(:reserve) { Reserve.create(city: 'Moz', reserved_date: '01-06-2022', user_id: user.id, product_id: product.id) }
 
-
   describe 'GET /index' do
     before do
       sign_in user
@@ -17,6 +16,5 @@ RSpec.describe 'Reserves', type: :request do
     it 'should return status correct (ok)' do
       expect(response).to have_http_status('200')
     end
-
   end
 end
