d89f00501fcac9aa831dbd0c054b643a71dfbd78
Ishpaul Singh <104348397+ishpaul777@users.noreply.github.com>
2022-12-23 16:55:49 +0530

    added enviornment variable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -30,7 +30,7 @@ Devise.setup do |config|
 
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.secret = ENV['JWT_SECRET_KEY']
 
     jwt.dispatch_requests = [
       ['POST', %r{^/sign_in$}]
